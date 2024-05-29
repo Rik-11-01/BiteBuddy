e5c1f4601100c163a94e613228516e032a9da0fd
Ritik Raman <ritikraman01@gmail.com>
2024-05-30 00:39:14 +0530

    AccountView

---
+++ Appetizer/Screens/AccountView/AccountViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AccountViewModel.swift
+//  Appetizer
+//
+//  Created by Ritik Raman on 29/05/24.
+//
+
+import Foundation
---
+++ Appetizer/Utilities/Extension/String+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  String+Ext.swift
+//  Appetizer
+//
+//  Created by Ritik Raman on 29/05/24.
+//
+
+import Foundation
