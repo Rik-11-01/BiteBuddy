529a2e9db110dfe52d48ec059ff6e215fe414e59
Ritik Raman <ritikraman01@gmail.com>
2024-05-29 13:41:55 +0530

    CardView

---
+++ Appetizer/Screens/Appetizerlistview/AppetizerDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  AppetizerDetailView.swift
+//  Appetizer
+//
+//  Created by Ritik Raman on 29/05/24.
+//
+
+import SwiftUI
+
+struct AppetizerDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppetizerDetailView()
+}
---
+++ Appetizer/View/Image/buttons/APxdismissbutton.swift
@@ -0,0 +1,18 @@
+//
+//  APxdismissbutton.swift
+//  Appetizer
+//
+//  Created by Ritik Raman on 29/05/24.
+//
+
+import SwiftUI
+
+struct APxdismissbutton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    APxdismissbutton()
+}
