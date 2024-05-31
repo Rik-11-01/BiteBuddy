7aa6771b7b90d9c5507671d2b6aab707587bdd96
Ritik Raman <ritikraman01@gmail.com>
2024-06-01 02:27:51 +0530

    EmptyState in order

---
+++ Appetizer/View/EmptyState/EmptyStates.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyStates.swift
+//  Appetizer
+//
+//  Created by Ritik Raman on 01/06/24.
+//
+
+import SwiftUI
+
+struct EmptyStates: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyStates()
+}
